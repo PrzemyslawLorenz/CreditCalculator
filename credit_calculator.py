51c1eb7dfc56ff4b4b1e7edc5b23040c37fd6fcd
unknown <przemoleko@gmail.com>
2020-05-14 11:42:33 +0200

    General program outline

--- credit_calculator.py
+++ credit_calculator.py
@@ -1,3 +1,12 @@
 """
     This program can be used to calculate loan costs and to print schedule
 """
+def equal_payments(loanAmount, loanTerm, interestRate, downPayment):
+    pass
+
+def decreasing_payments(loanAmount, loanTerm, interestRate, downPayment):
+    pass
+
+
+equal_payments(200000, 24, 0.05, 0.1)
+decreasing_payments(200000, 24, 0.05, 0.1)
