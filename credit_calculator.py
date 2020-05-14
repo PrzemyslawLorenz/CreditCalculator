6580b88e55022e3c4072d4dfd6b12c3a58141eae
unknown <przemoleko@gmail.com>
2020-05-14 13:24:30 +0200

    Added shedule (via terminal)

--- credit_calculator.py
+++ credit_calculator.py
@@ -7,27 +7,46 @@ def equal_payments(loanAmount, loanTerm, interestRate, downPayment):
     loanAmount += loanAmount * downPayment
     ratePerPeriod = ((loanAmount * i * (1 + i)**loanTerm) / ((1 + i)**loanTerm - 1))
 
-    for n in reversed(range(1, loanTerm + 1)):
-        capital = ratePerPeriod / (1 + i)**n
+    print("\n\tSCHEDULE OF EQUAL PAYMENTS\n")
+    print("-" * 77)
+    print("|  {:14}  |  {:14}  |  {:14}  |  {:14}  |" .format("Payment nr.", "Capital", "Interest", "Total"))
+    print("-" * 77)
+
+    for n in range(loanTerm):
+        capital = ratePerPeriod / (1 + i)**loanTerm
         interest = ratePerPeriod - capital
         totalInterestPaid += interest
+        loanTerm -= 1
+        print("|  {:14}  |  {:14.2f}  |  {:14.2f}  |  {:14.2f}  |" .format(n+1, round(capital, 2), round(interest, 2), round(ratePerPeriod, 2)))
 
-    print("Rate per period:", ratePerPeriod)
-    print("Total interest paid:", totalInterestPaid)
-    print("Total cost of all payments:", loanAmount + totalInterestPaid)
+    print("-" * 77)
+    print("|  {:14}  |  {:14}  |  {:14}  |  {:14}  |" .format("Loan Term", "Loan Amount", "Total interest", "Total costs"))
+    print("-" * 77)
+    print("|  {:14}  |  {:14.2f}  |  {:14.2f}  |  {:14.2f}  |" .format(n+1, round(loanAmount, 2), round(totalInterestPaid, 2), round(loanAmount + totalInterestPaid, 2)))
+    print("-" * 77)
+
 
 def decreasing_payments(loanAmount, loanTerm, interestRate, downPayment):
     totalInterestPaid = 0
     i = interestRate / 12   # i = Interest rate per period assuming 12 periods per year (monthly payment)
     loanAmount += loanAmount * downPayment
 
+    print("\n\tSCHEDULE OF DECREASING PAYMENTS\n")
+    print("-" * 77)
+    print("|  {:14}  |  {:14}  |  {:14}  |  {:14}  |" .format("Payment nr.", "Capital", "Interest", "Total"))
+    print("-" * 77)
+
     for n in range(loanTerm):
         capital = loanAmount / loanTerm
         interest = (loanAmount - (capital * n)) * i
         totalInterestPaid += interest
+        print("|  {:14}  |  {:14.2f}  |  {:14.2f}  |  {:14.2f}  |" .format(n+1, round(capital, 2), round(interest, 2), round(capital + interest, 2)))
 
-    print("Total interest paid:", totalInterestPaid)
-    print("Total cost of all payments:", loanAmount + totalInterestPaid)
+    print("-" * 77)
+    print("|  {:14}  |  {:14}  |  {:14}  |  {:14}  |" .format("Loan Term", "Loan Amount", "Total interest", "Total costs"))
+    print("-" * 77)
+    print("|  {:14}  |  {:14.2f}  |  {:14.2f}  |  {:14.2f}  |" .format(n+1, round(loanAmount, 2), round(totalInterestPaid, 2), round(loanAmount + totalInterestPaid, 2)))
+    print("-" * 77)
 
 
 equal_payments(200000, 24, 0.05, 0.1)
