b67a242b4edefb33f3e380fc606da0ce7c46f22e
unknown <przemoleko@gmail.com>
2020-05-14 12:46:50 +0200

    Added "decreasing_payments" function

--- credit_calculator.py
+++ credit_calculator.py
@@ -14,10 +14,20 @@ def equal_payments(loanAmount, loanTerm, interestRate, downPayment):
 
     print("Rate per period:", ratePerPeriod)
     print("Total interest paid:", totalInterestPaid)
-    print("Total cost of all payments:", ratePerPeriod * loanTerm)
+    print("Total cost of all payments:", loanAmount + totalInterestPaid)
 
 def decreasing_payments(loanAmount, loanTerm, interestRate, downPayment):
-    pass
+    totalInterestPaid = 0
+    i = interestRate / 12   # i = Interest rate per period assuming 12 periods per year (monthly payment)
+    loanAmount += loanAmount * downPayment
+
+    for n in range(loanTerm):
+        capital = loanAmount / loanTerm
+        interest = (loanAmount - (capital * n)) * i
+        totalInterestPaid += interest
+
+    print("Total interest paid:", totalInterestPaid)
+    print("Total cost of all payments:", loanAmount + totalInterestPaid)
 
 
 equal_payments(200000, 24, 0.05, 0.1)
