83e4c2da1b97c4d788d459303f1c754ef115ed3e
unknown <przemoleko@gmail.com>
2020-05-14 12:39:15 +0200

    Added "equal_payments" function

--- credit_calculator.py
+++ credit_calculator.py
@@ -2,7 +2,19 @@
     This program can be used to calculate loan costs and to print schedule
 """
 def equal_payments(loanAmount, loanTerm, interestRate, downPayment):
-    pass
+    totalInterestPaid = 0
+    i = interestRate / 12   # i = Interest rate per period assuming 12 periods per year (monthly payment)
+    loanAmount += loanAmount * downPayment
+    ratePerPeriod = ((loanAmount * i * (1 + i)**loanTerm) / ((1 + i)**loanTerm - 1))
+
+    for n in reversed(range(1, loanTerm + 1)):
+        capital = ratePerPeriod / (1 + i)**n
+        interest = ratePerPeriod - capital
+        totalInterestPaid += interest
+
+    print("Rate per period:", ratePerPeriod)
+    print("Total interest paid:", totalInterestPaid)
+    print("Total cost of all payments:", ratePerPeriod * loanTerm)
 
 def decreasing_payments(loanAmount, loanTerm, interestRate, downPayment):
     pass
